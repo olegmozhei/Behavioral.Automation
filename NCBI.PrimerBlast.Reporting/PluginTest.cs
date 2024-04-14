5be019933ca8fe5fa1f32e029508860eea725116
OlegMozhey <oleg.mozhey@quantori.com>
2024-04-14 18:49:26 +0400

    temp

--- NCBI.PrimerBlast.Reporting/PluginTest.cs
+++ NCBI.PrimerBlast.Reporting/PluginTest.cs
@@ -1,7 +1,17 @@
-﻿namespace NCBI.PrimerBlast.Reporting
+﻿using NCBI.PrimerBlast.Reporting;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: RuntimePlugin(typeof(PluginTest))]
+
+namespace NCBI.PrimerBlast.Reporting
 {
-    public class PluginTest
+    public class PluginTest : IRuntimePlugin
     {
-        
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
